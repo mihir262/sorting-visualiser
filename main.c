ac83e2816b71d96166a9a36087c9d67c9a557525
mihir <mihirdeogaonkar@gmail.com>
2026-04-12 17:14:14 +0530

    add gitignore file and drawRectangles() funtion

--- main.c
+++ main.c
@@ -4,16 +4,34 @@
 #define WIDTH 900
 #define HEIGHT 600
 
+#define COUNT 10
+
+int numbers[COUNT];
+
+void drawRectangles(){
+    for(int i = 0; i < COUNT; i++){
+        int value = numbers[i];
+        DrawRectangle(i * 50, HEIGHT/2, 20, value, RAYWHITE);
+    }
+}
+    
+    
+
 int main(){
     
+    for(int i = 0; i < COUNT; i++){
+        numbers[i] = i;
+    }
+
     InitWindow(WIDTH, HEIGHT, "Sorting Visualiser");
 
     while(!WindowShouldClose()){
         BeginDrawing();
+        
+        drawRectangles();
 
         EndDrawing();
     }
-    
     return 0;
 }
 
