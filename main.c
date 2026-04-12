edb69bc49a57be24c61cf29c48c278c2ffb312ab
mihir <mihirdeogaonkar@gmail.com>
2026-04-12 11:38:28 +0530

    initial commit

---
+++ main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <raylib.h>
+
+#define WIDTH 900
+#define HEIGHT 600
+
+int main(){
+    
+    InitWindow(WIDTH, HEIGHT, "Sorting Visualiser");
+
+    while(!WindowShouldClose()){
+        BeginDrawing();
+
+        EndDrawing();
+    }
+    
+    return 0;
+}
+
